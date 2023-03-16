82fa3713acae1624bd3315e3bb8ae8806c86cff5
vucongtuyen221 <vucongtuyen221@gmail.com>
2023-03-17 00:25:21 +0700

    upload

---
+++ bai8.js
@@ -0,0 +1,7 @@
+
+x ="qua tao"
+x = document.queryslector(".n");
+x.addeventslistener("click",function(){console.log("bạn vừa click")});
+// $(".n").click(function(){console.log("bạn vừa click vào tôi!")})
+
+
